f99c8c36ef94fd14e9251d857f7b2072fa6051f0
Carol Nichols <carol.nichols@gmail.com>
2015-01-15 21:11:59 -0500

    Modify the PATH for Aruba's processes to include cargo target
    Now we can run the command we're testing nice and pretty

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,1 +1,4 @@
 require 'aruba/cucumber'
+
+# Add rust's `target` directory to the PATH so we can just run the binary
+ENV['PATH'] = "#{File.expand_path('target')}#{File::PATH_SEPARATOR}#{ENV['PATH']}"
