04c20cb3c532c1acc2f9196ec769f436f0134460
Nick Quaranto <nick@quaran.to>
2015-01-21 23:18:51 -0500

    Got ssh cloning working...kind of

--- src/commands/start.rs
+++ src/commands/start.rs
@@ -1,10 +1,21 @@
 extern crate git2;
 
+use std::cell::RefCell;
+use std::io::stdio;
+
+struct State {
+    progress: Option<git2::Progress<'static>>,
+    total: usize,
+    current: usize,
+    path: Path,
+    newline: bool,
+}
+
 pub fn run(args: &[String]) {
     match args.first() {
         Some(p) => {
             match regex!(r"(://|@)").is_match(p.as_slice()) {
-                true  => clone_port(p),
+                true  => start_port(p),
                 false => create_new_port(p.as_slice()),
             };
         },
@@ -12,11 +23,35 @@ pub fn run(args: &[String]) {
     }
 }
 
-fn clone_port(url: &String) {
+fn start_port(url: &String) {
     // TODO: support cloning into a directory by taking more args
-    match git2::Repository::clone(url.as_slice(), &Path::new(humanish_url_part(url.as_slice()))) {
+
+    let state = RefCell::new(State {
+        progress: None,
+        total: 0,
+        current: 0,
+        path: Path::new("."),
+        newline: false,
+    });
+
+    let mut cb = git2::RemoteCallbacks::new();
+    cb.transfer_progress(|stats| {
+        let mut state = state.borrow_mut();
+        state.progress = Some(stats.to_owned());
+        print(&mut *state);
+        true
+    });
+
+    cb.credentials(|url, username_from_url, allowed_types| {
+        println!("url {}", url);
+        // TODO obviously dont hardcode this, need to look up more stuff from Cred's
+        git2::Cred::ssh_key("git", Some(&Path::new("/Users/qrush/.ssh/id_rsa.pub")), &Path::new("/Users/qrush/.ssh/id_rsa"), None)
+    });
+
+    match git2::build::RepoBuilder::new().remote_callbacks(cb)
+        .clone(url.as_slice(), &Path::new(humanish_url_part(url.as_slice()))) {
         Ok(_)    => {},
-        Err(msg) => println!("Could not clone a port: {}", msg),
+        Err(msg) => println!("Could not start a port: {}", msg),
     };
 }
 
@@ -34,4 +69,33 @@ fn humanish_url_part(url: &str) -> &str {
     let last_part = pieces.last().unwrap_or(&url);
     let name_pieces: Vec<&str> = last_part.split('.').collect();
     *name_pieces.first().unwrap_or(last_part)
+}
+
+// Very much copied from https://github.com/alexcrichton/git2-rs
+fn print(state: &mut State) {
+    let stats = state.progress.as_ref().unwrap();
+    let network_pct = (100 * stats.received_objects()) / stats.total_objects();
+    let index_pct = (100 * stats.indexed_objects()) / stats.total_objects();
+    let co_pct = if state.total > 0 {
+        (100 * state.current) / state.total
+    } else {
+        0
+    };
+    let kbytes = stats.received_bytes() / 1024;
+    if stats.received_objects() == stats.total_objects() && false {
+        if !state.newline {
+            println!("");
+            state.newline = true;
+        }
+        print!("Resolving deltas {}/{}\r", stats.indexed_deltas(),
+               stats.total_deltas());
+    } else {
+        print!("net {:3}% ({:4} kb, {:5}/{:5})  /  idx {:3}% ({:5}/{:5})  \
+                /  chk {:3}% ({:4}/{:4}) {}\r",
+               network_pct, kbytes, stats.received_objects(),
+               stats.total_objects(),
+               index_pct, stats.indexed_objects(), stats.total_objects(),
+               co_pct, state.current, state.total, state.path.display());
+    }
+    stdio::flush();
 }
